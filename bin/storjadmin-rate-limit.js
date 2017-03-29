ec7ff451bef1ac1ea2d2c7de4aeb088fbeb3fa05
Patrick Gerbes <patrickgerbes@gmail.com>
2017-03-29 18:28:06 -0400

    Add command for setting user rate limit flag

--- bin/storjadmin-rate-limit.js
+++ bin/storjadmin-rate-limit.js
@@ -17,3 +17,31 @@ if (!rate_limit.user) {
   console.error('\n  missing user, try --help');
   process.exit(1);
 }
+
+const storage  = new Storage(mongo.url, mongo.opts);
+
+let User = storage.models.User;
+
+if (rate_limit.off) {
+  User.findByIdAndUpdate(
+  	rate_limit.user,
+    {
+      $set: {
+        isFreeTier: false
+  	  }
+    }, (err, user) => {
+  	if (err) {
+      log('error', 'Failed on user update, reason %s',
+  		  err.message);
+      process.exit(1);
+  	}
+
+  	if (!user) {
+  		log('error', 'User not found in db');
+  		process.exit(1);
+  	}
+  	log('info', 'User rate limit successfully updated: %s',
+  		user);
+  	process.exit(1);
+  });
+}

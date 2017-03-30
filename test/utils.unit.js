0d6aa2a8ea6dd100405d12ac271e211572758dba
Patrick Gerbes <patrickgerbes@gmail.com>
2017-03-30 13:49:58 -0400

    Fix spacing and semicolon

--- test/utils.unit.js
+++ test/utils.unit.js
@@ -11,18 +11,21 @@ describe('utils functions', function() {
       	utils.addYearsToCurrentDate('one');
       }).to.throw(Error);
     });
+
     it('should return a date', function() {
       expect(utils.addYearsToCurrentDate(1)).to.be.an.instanceOf(Date);
-    })
+    });
   });
 
   describe('#setOptionFlag', function() {
     const command = {
     	flag: true
     };
+
     it('should set undefined flags to false', function() {
       expect(utils.setOptionFlag(command.noFlag)).to.equal(false);
     });
+    
     it('should set true flags to true', function() {
     	expect(utils.setOptionFlag(command.flag)).to.equal(true);
     });

26006844d818d9fa8c98c816e8cc9b1c2d90dfe6
Patrick Gerbes <patrickgerbes@gmail.com>
2017-03-30 13:50:27 -0400

    Add unit test stubs for credits module

---
+++ test/credits.unit.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const credits = require('../lib/utils');
+const {expect} = require('chai');
+
+describe('Credit functions', function() {
+
+  describe('#addManualCredit', function() {
+    it('should log error with no user argument', function() {
+   
+    });
+
+    it('should log error with no amount argument', function() {
+
+    });
+
+    it('should log error if user does not exist', function() {
+
+    });
+
+    it('should log error if credit insert fails', function() {
+
+    });
+
+    it('should log credit data on insert success', function() {
+
+    });
+  });
+});

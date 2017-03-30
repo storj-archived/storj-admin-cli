d1a335576465fe3c2f242f3f084ca3b8d9924eba
Patrick Gerbes <patrickgerbes@gmail.com>
2017-03-30 12:30:39 -0400

    Add test for setOptionFlag

--- test/utils.unit.js
+++ test/utils.unit.js
@@ -6,11 +6,22 @@ const {expect} = require('chai');
 describe('utils functions', function() {
 
   describe('#addYearsToCurrentDate', function() {
-
     it('should fail for non numeric input', function() {
       expect(function () {
       	utils.addYearsToCurrentDate('one');
       }).to.throw(Error);
+    });
+  });
+
+  describe('#setOptionFlag', function() {
+    const command = {
+    	flag: true
+    };
+    it('should set undefined flags to false', function() {
+      expect(utils.setOptionFlag(command.noFlag)).to.equal(false);
+    });
+    it('should set true flags to true', function() {
+    	expect(utils.setOptionFlag(command.flag)).to.equal(true);
     });
   });
 });

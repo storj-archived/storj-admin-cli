f7c886f3535feaec9d1626174214c682116ef3db
Patrick Gerbes <patrickgerbes@gmail.com>
2017-03-30 13:50:42 -0400

    Add unit test stubs for users module

---
+++ test/users.unit.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const users = require('../lib/credits');
+const {expect} = require('chai');
+
+describe('User functions', function() {
+
+  describe('#setUserRateLimitFlag', function() {
+    it('should log error with no user argument', function() {
+   
+    });
+
+    it('should log error if user does not exist', function() {
+
+    });
+
+    it('should log user data on update success', function() {
+
+    });
+
+    it('should set user.isFreeTier to false if --off passed', function() {
+
+    });
+
+    it('should set user.isFreeTier to true if --off not passed', function() {
+
+    });
+  });
+});
